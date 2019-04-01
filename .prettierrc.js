bd38f37b67863e01c6426ed5da9c897c8570cd44
李蔚生 <971353677@qq.com>
2019-04-01 11:05:57 +0800

    chore: create development environment

---
+++ .prettierrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: false,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'avoid',
+  requirePragma: false,
+  insertPragma: false,
+  proseWrap: 'preserve',
+  htmlWhitespaceSensitivity: 'css',
+  endOfLine: 'lf',
+};
---
+++ README.md
@@ -0,0 +1,3 @@
+# Fedlinker · [![lerna](https://img.shields.io/badge/maintained%20with-lerna-cc00ff.svg)](https://lernajs.io/)
+
+Tools for building React project with zero configuration as possible.
